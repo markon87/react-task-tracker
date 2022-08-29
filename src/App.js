5a516fa03a99b55aca7ba380d798be5ad4f468e4
Marko Novakovic <marko.novakovic@glimmernet.com>
2022-08-29 16:32:15 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
+import Footer from "./components/Footer";
 import Tasks from './components/Tasks';
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 function App() {
   const [ showAddTask, setShowAddTask] = useState(false);
@@ -82,14 +85,26 @@ const toggleReminder = async (id) => {
 };
 
   return (
+    <Router>
     <div className='container'>
       <Header onAdd={() => {setShowAddTask(!showAddTask)}}
       showAdd={showAddTask} />
-     {showAddTask && <AddTask onAdd={ addTask } />}
-      {tasks.length > 0 ? 
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
-      : 'No Tasks'}
+      <Routes>
+        <Route path='/' exact render={(props) => (
+          <>
+          {showAddTask && <AddTask onAdd={ addTask } />}
+          {tasks.length > 0 ? 
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
+          : 'No Tasks'}
+          </>
+        )} />
+      </Routes>
+      <Routes>
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
